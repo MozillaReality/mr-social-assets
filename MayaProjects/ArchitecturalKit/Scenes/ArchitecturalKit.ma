0ac49bf9ad4a0e3b884ef1a6d192063a6805cc43
Robert Long <robert@robertlong.me>
2018-06-25 14:15:34 -0700

    Update export script to freeze transforms correctly.

--- MayaProjects/ArchitecturalKit/Scenes/ArchitecturalKit.ma
+++ MayaProjects/ArchitecturalKit/Scenes/ArchitecturalKit.ma
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85e29458a8cf873f87ab29b2e6d2fd7fc2bc5409055b6529a29b9d78cd7a975e
-size 914328
+oid sha256:5ca80c40ff0552b3245b91b1fac9dff1df7cf1e1ae285a5b86572a24ffe6c0bc
+size 910271
--- MayaProjects/scripts/ExportSelectedAsGLTF.py
+++ MayaProjects/scripts/ExportSelectedAsGLTF.py
@@ -10,4 +10,9 @@ selectedPaths = cmds.fileDialog2(fileMode=2)
 
 for objName in selectedObjectNames:
 	cmds.select(objName)
-	cmds.maya2glTF(outputFolder=selectedPaths[0], sceneName=objName)
+	pivot = cmds.getAttr(".rotatePivot")[0]
+	cmds.setAttr(".translate", -pivot[0], -pivot[1], -pivot[2])
+	cmds.makeIdentity(apply=True, t=1, r=1, s=1, n=0, pn=1)
+	cmds.maya2glTF(outputFolder=selectedPaths[0], sceneName=objName, scaleFactor=0.01)
+	cmds.setAttr(".translate", pivot[0], pivot[1], pivot[2])
+	cmds.makeIdentity(apply=True, t=1, r=1, s=1, n=0, pn=1)
